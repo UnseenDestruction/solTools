93a685cd71a11c74940c620823fd611c02ad1777
Unseen <unseenworkmail@gmail.com>
2023-12-26 23:04:57 +0800

    commit

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -5,9 +5,11 @@ const Footer = () => {
 
     return(
         <div className="display flex justify-between items-center bg-black p-10 ">   
-           <h1>Copyright @Pondini Development, 2023</h1>      
-           <a href=""><BsDiscord /></a>
-           <a href=""><FaTwitter /></a>
+           <h1>Copyright @Pondini Development, 2023</h1>  
+           <div className="text-lg display flex gap-4">    
+           <a href="" className="hover:-translate-y-2 duration-300"><BsDiscord /></a>
+           <a href="" className="hover:-translate-y-2 duration-300"><FaTwitter /></a>
+           </div>
         </div>
     )
 }
