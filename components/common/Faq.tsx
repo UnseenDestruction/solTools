678a075a9d2f605e7e51073b73741bfb36151df7
Unseen <unseenworkmail@gmail.com>
2023-12-26 22:40:06 +0800

    commit

--- components/common/Faq.tsx
+++ components/common/Faq.tsx
@@ -8,19 +8,19 @@ const Faq = () => {
     const faqData = [
         {
             question: "What is this Website?",
-            answer: "This website is a token creator, token manager, and market creator website...",
+            answer: "This website is a token creator, token manager, and market creator website! We noticed that people often struggle trying to figure out all the different things to do to make a token on the solana network, so we've made it easy for you where it's all available to you in one convenient website! Read our <guide link> for learning about all the steps to launch a token!",
         },
         {
             question: "I Want To Launch A Token But I Don't Know How!",
-            answer: "Please read our Guide to learn and understand how you can create a solana token...",
+            answer: "Please read our Guide Page to learn and understand how you can create a solana token and make it ready to trade on the market from start to finish!",
         },
         {
             question: "Are my coins safe/Is this website safe?",
-            answer: "Yes, this is a dApp and does only ensure you are the token owner, mint and freeze authority...",
+            answer: "Yes, this is a dApp and does only ensures you are the token owner, mint and freeze authority, and so on. Reviews left on our discord server(links) as well can show you that we are not here to scam!",
         },
         {
             question: "What is the fee for using your platform?",
-            answer: "We charge a flat rate fee of 0.1 solana for token creation...",
+            answer: "We charge a flat rate fee of 0.1 solana for token creation. We charge a flat rate fee of 0.5 solana for market creation. Market creation is often deemed to cost 3 sol, but it really doesn't have to be! Our market creation system uses custom values that reduces the cost on your end. We do NOT charge anything for revoking mint authority and freeze authority! We're just cool like that",
         },
         {
             question: "What wallets can I use?",
